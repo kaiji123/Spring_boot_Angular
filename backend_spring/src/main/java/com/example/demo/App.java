f1be1dd56474178c2ab98ea4c436a5498c5c9c45
Kai <kejinko012@gmail.com>
2021-08-18 02:20:04 +0200

    hello

---
+++ backend_spring/src/main/java/com/example/demo/App.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+import com.example.demo.entities.User;
+import com.example.demo.repositories.UserRepository;
+import java.util.stream.Stream;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class App{
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    CommandLineRunner init(UserRepository userRepository) {
+        return args -> {
+            Stream.of("John", "Julie", "Jennifer", "Helen", "Rachel").forEach(name -> {
+                User user = new User(name, name.toLowerCase() + "@domain.com");
+                userRepository.save(user);
+            });
+            userRepository.findAll().forEach(System.out::println);
+        };
+    }
+    
+  
+}
---
+++ backend_spring/src/main/java/com/example/demo/configs/WebConfig.java
@@ -0,0 +1,15 @@
+package com.example.demo.configs;
+
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements  WebMvcConfigurer {
+	   @Override
+       public void addCorsMappings(CorsRegistry registry) {
+           registry.addMapping("/*").
+           allowedOrigins("*").
+           allowedMethods("*").
+           allowedHeaders("*").
+           allowCredentials(true);
+       }
+}
